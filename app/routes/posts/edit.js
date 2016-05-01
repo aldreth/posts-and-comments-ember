11075aa7fb5421e47596445c948b57a0ccfb4696
Edward Andrews-Hodgson <edward.andrews-hodgson@sky.uk>
2016-05-01 07:56:07 +0100

    Clear unsaved changes when leaving new & edit

--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.createRecord('post');
-  },
-
   deactivate() {
     let model = this.controller.get('model');
     model.rollbackAttributes();
